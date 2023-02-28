b0459b8b2704213b5db31dd7e911441aee46ac63
Dorian Devecz <dorian.devecz@outlook.com>
2023-02-28 08:19:02 +0100

    Dotnet programmind

---
+++ programozas/02.22 - KaracsonyiDiszek/KarácsonyiCLI/KarácsonyiCLI/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace KarácsonyiCLI
+{
+    class Program
+    {
+        class NapiMunka
+        {
+            public static int KeszultDb { get; private set; }
+            public int Nap { get; private set; }
+            public int HarangKesz { get; private set; }
+            public int HarangEladott { get; private set; }
+            public int AngyalkaKesz { get; private set; }
+            public int AngyalkaEladott { get; private set; }
+            public int FenyofaKesz { get; private set; }
+            public int FenyofaEladott { get; private set; }
+
+            public NapiMunka(string sor)
+            {
+                string[] s = sor.Split(';');
+                Nap = Convert.ToInt32(s[0]);
+                HarangKesz = Convert.ToInt32(s[1]);
+                HarangEladott = Convert.ToInt32(s[2]);
+                AngyalkaKesz = Convert.ToInt32(s[3]);
+                AngyalkaEladott = Convert.ToInt32(s[4]);
+                FenyofaKesz = Convert.ToInt32(s[5]);
+                FenyofaEladott = Convert.ToInt32(s[6]);
+
+                NapiMunka.KeszultDb += HarangKesz + AngyalkaKesz + FenyofaKesz;
+            }
+
+            public int NapiBevetel()
+            {
+                return -(HarangEladott * 1000 + AngyalkaEladott * 1350 + FenyofaEladott * 1500);
+            }
+        }
+
+        static List<NapiMunka> tarolo = new List<NapiMunka>();
+
+        static void beolvas()
+        {
+            StreamReader olvas = new StreamReader("diszek.txt");
+            string sor;
+            NapiMunka egynap;
+
+            while (!olvas.EndOfStream)
+            {
+                sor = olvas.ReadLine();
+                egynap = new NapiMunka(sor);
+                tarolo.Add(egynap);
+            }
+            olvas.Close();
+            olvas.Dispose();
+            egynap = null;
+        }
+
+        static void fel4()
+        {
+            int osszeg = 0;
+                foreach(NapiMunka item in tarolo)
+            {
+                osszeg += item.AngyalkaKesz + item.FenyofaKesz + item.HarangKesz;
+            }
+            Console.WriteLine("4. Feladat: Összesen {0} darab készült.", NapiMunka.KeszultDb);   
+        }
+
+        static void fel5()
+        {
+            int osszeg = 0;
+            foreach (NapiMunka item in tarolo)
+            {
+                osszeg += item.AngyalkaKesz + item.FenyofaKesz + item.HarangKesz;
+                if(osszeg==0)
+                {
+                    Console.WriteLine("5. Feladat: Volt olyan nap, amikor egyetlen disz sem készült.\n");
+                    break;
+                }
+            }
+        }
+
+        static void fel6()
+        {
+            Console.WriteLine("6.Feladat");
+            int nap = 0;
+            do
+            {
+                Console.Write("Adja meg a keresett napot [1...40]:");
+                nap = Convert.ToInt32(Console.ReadLine());
+            } while (nap < 1 || nap > 40);
+
+            int angyal = 0;
+            int fenyo = 0;
+            int harang = 0;
+            foreach(NapiMunka item in tarolo)
+            {
+                angyal += item.AngyalkaKesz + item.AngyalkaEladott;
+                fenyo += item.FenyofaKesz + item.FenyofaEladott;
+                harang += item.HarangKesz + item.HarangEladott;
+
+                if (item.Nap >= nap)
+                    break;
+            }
+            Console.WriteLine("\tA(z) {0}. nap végén {1} harang, {2} angyalka és {3} fenyőfa maradt készleten.\n", nap, harang, fenyo, angyal);
+        }
+        static void Main(string[] args)
+        {
+            beolvas();
+            fel4();
+            fel5();
+            fel6();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ programozas/ValtozokGyakorlas/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ValtozokGyakorlas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Valtozok();
+            Naptar();
+            Filmcimek();
+        }
+
+        static void Valtozok()
+        {
+            // Változó deklarálása és értékadás külön sorban:
+            string maiNap;
+            maiNap = "hétfő";
+            maiNap = "kedd";
+            maiNap = "szerda";
+            Console.WriteLine("Mai nap: " + maiNap);
+
+            // Változók deklarálása és értékadás:
+            string vezeteknev = "Próba";
+            string keresztnev = "Elemér";
+            string teljesNev = vezeteknev + " " + keresztnev;
+            int iranyitoszam = 8900;
+            string[] tantarnyak = { "matematika", "irodalom", "programozás" };
+            bool tanulo = true;
+
+            Console.Write(teljesNev);
+            Console.WriteLine(" (irányítószám: " + iranyitoszam + ", tanulo: " + tanulo + ")");
+
+            // If-Else elágazás:
+            if (iranyitoszam == 8900)
+            {
+                Console.WriteLine("Zalaegerszegi");
+            }
+            else if (iranyitoszam == 8800)
+            {
+                Console.WriteLine("Nagykanizsai");
+            }
+            else
+            {
+                Console.WriteLine("Egyéb városból");
+            }
+
+            // Foreach ciklus:
+            Console.Write("Tantárgyak: ");
+            foreach (string tantargy in tantarnyak)
+            {
+                Console.Write(tantargy + ", ");
+            }
+            Console.WriteLine("\n");
+
+            // For ciklus:
+            for (int tantargySzama = 0; tantargySzama < tantarnyak.Length; tantargySzama++)
+            {
+                Console.WriteLine(tantargySzama + ": " + tantarnyak[tantargySzama]);
+            }
+
+            Console.WriteLine("\n-------------------------------\n");
+        }
+
+        static void Naptar()
+        {
+            string[] hetNapjai = { "hetfo", "kedd", "szerda", "csutortok", "pentek" };
+            Console.WriteLine("A hét első napja: " + hetNapjai[0]);
+            Console.WriteLine();
+
+            foreach (string nap in hetNapjai)
+            {
+                bool hetfoVan = (nap == "hetfo");
+                bool nincsHetfo = (nap != "hetfo");
+
+                Console.Write(nap);
+                Console.Write(" (hétfő van: " + hetfoVan + ")");
+                Console.WriteLine(" (nincs hétfő: " + nincsHetfo + ")");
+
+                if (nap == "hetfo")
+                {
+                    Console.WriteLine("- Meh hétfő!");
+                }
+                else if (nap == "pentek")
+                {
+                    Console.WriteLine("- Hurrá péntek van!");
+                }
+                else
+                {
+                    Console.WriteLine("- Nincs hétfő!");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("-------------------------------\n");
+        }
+
+        static void Filmcimek()
+        {
+            List<string> filmCimek = new List<string>() { "Alien", "Alien2", "Alien3" };
+            filmCimek.Add("Prometheus");
+
+            Console.WriteLine("Utolsó film:" + filmCimek.Last() + "\n");
+
+            foreach (string film in filmCimek)
+            {
+                bool regi = (film == "Alien" || film == "Alien2" || film == "Alien3");
+                bool uj = film == "Prometheus";
+
+                Console.Write(film);
+                Console.Write(" (régi: " + regi + ")");
+                Console.WriteLine(" (új: " + uj + ")");
+            }
+
+            Console.WriteLine("\n-------------------------------\n");
+        }
+    }
+}
